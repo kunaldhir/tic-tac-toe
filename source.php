76cb1a61a8f96f30965c96cdfb3a96be1f28e8e6
Kunal <kunaldhir@live.in>
2014-07-15 14:06:03 +0530

    Adding input form

--- source.php
+++ source.php
@@ -8,4 +8,14 @@ array(7, 8, 9)
 echo $table[0][0]."  ".$table[0][1]."  ".$table[0][2];
 echo $table[1][0]."  ".$table[1][1]."  ".$table[1][2];
 echo $table[2][0]."  ".$table[2][1]."  ".$table[2][2];
+
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+Put X at :<input type="text" name="input" placeholder="Type a number between 1 to 9">
+<input type="submit" value="Submit" name="Submit">
+</form>
+<?php
+
+
+
 ?>

68ba63da0844161edb92288a342cc6b245d6b55f
Kunal <kunaldhir@live.in>
2014-07-15 13:38:29 +0530

    Create source.php

---
+++ source.php
@@ -0,0 +1,11 @@
+<?
+$table = array
+(
+array(1, 2, 3),
+array(4, 5, 6),
+array(7, 8, 9)
+);
+echo $table[0][0]."  ".$table[0][1]."  ".$table[0][2];
+echo $table[1][0]."  ".$table[1][1]."  ".$table[1][2];
+echo $table[2][0]."  ".$table[2][1]."  ".$table[2][2];
+?>

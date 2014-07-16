e1f5eee8a7ed537940672b8a27c6face46e34400
Kunal <kunaldhir@live.in>
2014-07-16 12:09:08 +0530

    Update and rename source.php to tic-tac-toe.php

--- tic-tac-toe.php
+++ tic-tac-toe.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+$_SESSION['table']=$table;
+$_SESSION['input1']=$input1;
 $table = array
 (
 array(1, 2, 3),
@@ -19,6 +22,8 @@ Put X at :<input type="text" name="input1" placeholder="any no. between 1 to 9">
 <?php
 
 $input1 = $_POST['input1'];
+
+if(isset($_POST['submit'])&& $input1<10){
 
 if ($input1 == 1){
 $table[0][0] = "X";
@@ -56,36 +61,9 @@ elseif ($input1 == 9){
 $table[2][2] = "X";
 $table[1][1] = "O";
 }
-
-if(isset($_POST['submit']) && $input1<10){
-echo $table[0][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[0][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[0][2]."<br>"."<br>";
-echo $table[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][2]."<br>"."<br>";
-echo $table[2][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][2]."<br>"."<br>";
+header('location:input2.php');
 }
-elseif ($input1>9){
-echo "Type in a valid number.";
+elseif($input1>9) {
+	echo"Type in a valid number.";
 }
-
-?>
-
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-<?php
-echo "Put X at :"."<input type='text' name='input2' placeholder='any no. between 1 to 9'>";
-echo "<input type='submit' value='Submit' name='submit'>";
-echo "</form>"
-?>
-
-<?php
-
-$input2 = $_POST['input2'];
-
-if ($table[0][0] == "X" && $input2 == 3){
-	$table[0][2] = "X";
-	$table[0][1] = "O";
-}
-elseif ($table[0][0] == "X" && $input2 == 7){
-		$table[2][0] = "X";
-		$table[1][0] = "O";
-}
-
 ?>

7f2e0d23e796b86162a64f0d2d0232fb8bdb2d8c
Kunal <kunaldhir@live.in>
2014-07-18 17:10:02 +0530

    Update input4.php

--- input4.php
+++ input4.php
@@ -8,3 +8,8 @@ echo $table[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][1]."&nbsp;&nbsp;&nbsp;&nb
 echo $table[2][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][2]."<br>"."<br>";
 
 ?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+Put X at :<input type='text' name='input4' placeholder='any no. between 1 to 9'>
+<input type='submit' value='Submit' name='submit'>
+</form>

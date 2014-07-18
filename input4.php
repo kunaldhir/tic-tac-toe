2533a92f73eff58c92660748d3bd599eeeea2af0
Kunal <kunaldhir@live.in>
2014-07-18 16:46:44 +0530

    Create input4.php

---
+++ input4.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+$table=$_SESSION['table'];
+
+echo $table[0][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[0][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[0][2]."<br>"."<br>";
+echo $table[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][2]."<br>"."<br>";
+echo $table[2][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][2]."<br>"."<br>";
+
+?>

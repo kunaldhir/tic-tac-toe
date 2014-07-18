7ee168600091d84b9d936b2a737e3888d2a6b8d1
Kunal <kunaldhir@live.in>
2014-07-18 16:46:07 +0530

    Update input3.php

--- input3.php
+++ input3.php
@@ -19,51 +19,128 @@ $input3=$_POST['input3'];
 
 if(isset($_POST['submit'])&& $input2<10){
 	
-	if($table[0][0] == $table[0][1] == "X" && $input3 == 7)
+	if($table[0][0] == $table[0][1] == "X" && $input3 == 7){
 		$table[2][0] = "X";
-		$table[1][2] = "O";
+		$table[1][2] = "O";}
 	
 	//**************************Computer wins X = (1,2)***********************************
-	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 4)
+	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 4){
 		$table[1][0] = "X";
 		$table[2][0] = "O";
-        echo '<script>alert("Computer wins");window.location.tic.php;</script>';
-	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 6)
+        echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 6){
 		$table[1][2] = "X";
 		$table[2][0] = "O";
-        echo '<script>alert("Computer wins");window.location.tic.php;</script>';	
-	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 8)
+        echo '<script>alert("Computer wins");window.location.tic.php;</script>';}	
+	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 8){
 		$table[2][1] = "X";
 		$table[2][0] = "O";
-        echo '<script>alert("Computer wins");window.location.tic.php;</script>';
-	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 9)
+        echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[0][1] == "X" && $input3 == 9){
 		$table[2][2] = "X";
 		$table[2][0] = "O";
-        echo '<script>alert("Computer wins");window.location.tic.php;</script>';
+        echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
 	//************************************************************************************
 	
-	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 8)
+	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 8){
 		$table[2][1] = "X";
-		$table[1][0] = "O";
+		$table[1][0] = "O";}
 	
 	//**************************Computer wins X = (1,3)***********************************
-	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 4)
+	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 4){
 		$table[1][0] = "X";
 		$table[2][1] = "O";
-	echo '<script>alert("Computer wins");window.location.tic.php;</script>';
-	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 6)
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 6){
 		$table[1][2] = "X";
 		$table[2][1] = "O";
-	echo '<script>alert("Computer wins");window.location.tic.php;</script>';
-	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 7)
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 7){
 		$table[2][0] = "X";
 		$table[2][1] = "O";
-	echo '<script>alert("Computer wins");window.location.tic.php;</script>';
-	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 9)
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[0][2] == "X" && $input3 == 9){
 		$table[2][2] = "X";
 		$table[2][1] = "O";
-	echo '<script>alert("Computer wins");window.location.tic.php;</script>';
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
 	//************************************************************************************
-}
+	
+	elseif($table[0][0] == $table[1][0] == "X" && $input3 == 3){
+		$table[0][2] = "X";
+		$table[0][1] = "O";}
 
+	//**************************Computer wins X = (1,4)***********************************
+	elseif($table[0][0] == $table[1][0] == "X" && $input3 == 2){
+		$table[0][1] = "X";
+		$table[0][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][0] == "X" && $input3 == 6){
+		$table[1][2] = "X";
+		$table[0][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][0] == "X" && $input3 == 8){
+		$table[2][1] = "X";
+		$table[0][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][0] == "X" && $input3 == 9){
+		$table[2][2] = "X";
+		$table[0][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	//************************************************************************************
+
+	elseif($table[0][0] == $table[1][2] == "X" && $input3 == 7){
+		$table[2][0] = "X";
+		$table[0][1] = "O";}
+
+	//**************************Computer wins X = (1,6)***********************************
+	elseif($table[0][0] == $table[1][2] == "X" && $input3 == 2){
+		$table[0][1] = "X";
+		$table[2][0] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][2] == "X" && $input3 == 4){
+		$table[1][0] = "X";
+		$table[2][0] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][2] == "X" && $input3 == 8){
+		$table[2][1] = "X";
+		$table[2][0] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[1][2] == "X" && $input3 == 9){
+		$table[2][2] = "X";
+		$table[2][0] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	//************************************************************************************
+
+	elseif($table[0][0] == $table[2][0] == "X" && $input3 == 6){
+		$table[1][2] = "X";
+		$table[0][1] = "O";}
+	
+	//**************************Computer wins X = (1,7)***********************************
+	elseif($table[0][0] == $table[2][0] == "X" && $input3 == 2){
+		$table[0][1] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[2][0] == "X" && $input3 == 3){
+		$table[0][2] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[2][0] == "X" && $input3 == 8){
+		$table[2][1] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	elseif($table[0][0] == $table[2][0] == "X" && $input3 == 9){
+		$table[2][2] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");window.location.tic.php;</script>';}
+	//************************************************************************************
+
+	
+
+
+header('location:input4.php');
+}
+elseif($input1>9) {
+	echo"Type in a valid number.";
+}
+$_SESSION['table']=$table;
 ?>

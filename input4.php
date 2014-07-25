8e4419ee7a68a164401131371acc0aca20447b15
Kunal <kunaldhir@live.in>
2014-07-25 10:58:01 +0530

    Update input4.php

--- input4.php
+++ input4.php
@@ -13,3 +13,54 @@ echo $table[2][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][1]."&nbsp;&nbsp;&nbsp;&nb
 Put X at :<input type='text' name='input4' placeholder='any no. between 1 to 9'>
 <input type='submit' value='Submit' name='submit'>
 </form>
+
+<?php
+
+$input4=$_POST['input4'];
+
+
+if(isset($_POST['submit'])&& $input3<10){
+	
+	if($table[0][0] == "X" && $table[0][1] == "X" && $table[1][2] =="X" && $input4 == 6){
+		$table[1][2] = "X";
+		$table[2][2] = "O";
+		echo '<script>alert("Match Drawn");</script>';}
+	
+	//**************************Computer wins X = (1,2,7)***********************************
+	elseif($table[0][0] == "X" && $table[0][1] == "X" && $table[1][2] =="X" && $input4 == 8){
+		$table[2][1] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");</script>';}
+	elseif($table[0][0] == "X" && $table[0][1] == "X" && $table[1][2] =="X" && $input4 == 9){
+		$table[2][2] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");</script>';}
+	//************************************************************************************
+	
+	elseif($table[0][0] == "X" && $table[0][2] == "X" && $table[2][1] =="X" && $input4 == 6){
+		$table[1][2] = "X";
+		$table[2][2] = "O";
+		echo '<script>alert("Match Drawn");</script>';}
+
+	//**************************Computer wins X = (1,3,8)***********************************
+	elseif($table[0][0] == "X" && $table[0][2] == "X" && $table[2][1] =="X" && $input4 == 7){
+		$table[2][0] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");</script>';}
+	elseif($table[0][0] == "X" && $table[0][2] == "X" && $table[2][1] =="X" && $input4 == 9){
+		$table[2][2] = "X";
+		$table[1][2] = "O";
+		echo '<script>alert("Computer wins");</script>';}
+	//************************************************************************************
+
+
+
+echo '<script>window.location="http://localhost/input4.php"</script>';
+
+
+}
+elseif($input4>9) {
+	echo"Type in a valid number.";
+}
+$_SESSION['table']=$table;
+?>

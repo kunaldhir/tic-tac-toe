78769de85fa6cf2f7e2d89e002ffc312da6ebb67
Kunal <kunaldhir@live.in>
2014-08-01 12:56:23 +0530

    Update output.php

--- output.php
+++ output.php
@@ -9,6 +9,6 @@ echo $table[1][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[1][1]."&nbsp;&nbsp;&nbsp;&nb
 echo $table[2][0]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][1]."&nbsp;&nbsp;&nbsp;&nbsp;".$table[2][2]."<br>"."<br>";
 
 session_destroy();
-echo"<a href="tic-tac-toe.php">Play again</a>";
+echo"<a href='tic-tac-toe.php'>Play again</a>";
 
 ?>
